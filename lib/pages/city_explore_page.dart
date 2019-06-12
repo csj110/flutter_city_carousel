db7bd695aa42b12052c32563578a0bd0b51ee157
Cai Shengjian <1294797029@qq.com>
2019-06-12 10:09:54 +0800

    bgColor animation and descText animation

--- lib/pages/city_explore_page.dart
+++ lib/pages/city_explore_page.dart
@@ -1,6 +1,7 @@
+// import 'package:city_carousel/model/Detail.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:city_carousel/model/Detail.dart';
+import 'package:city_carousel/model/Detail.dart';
 
 final imagesList = [
   "assets/images/newyork.jpg",
@@ -27,7 +28,7 @@ class _CityExplorePageState extends State<CityExplorePage> {
     _pageController = PageController(
       initialPage: currentPage,
       keepPage: true,
-      viewportFraction: 0.5,
+      viewportFraction: 0.7,
     );
   }
 
@@ -40,17 +41,94 @@ class _CityExplorePageState extends State<CityExplorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        itemBuilder: (context, index) {
-          return itemBuilder(index);
-        },
-        controller: _pageController,
-        pageSnapping: true,
-        onPageChanged: _onPageChanged,
-        itemCount: 3,
-        physics: ClampingScrollPhysics(),
+      body: Stack(
+        children: <Widget>[
+          AnimatedContainer(
+            duration: Duration(microseconds: 500),
+            color: colorList[currentPage],
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                height: 450.0,
+                child: PageView.builder(
+                  itemBuilder: (context, index) {
+                    return itemBuilder(index);
+                  },
+                  controller: _pageController,
+                  pageSnapping: true,
+                  onPageChanged: _onPageChanged,
+                  itemCount: 3,
+                  physics: ClampingScrollPhysics(),
+                ),
+              ),
+              _detailBuilder(currentPage),
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _detailBuilder(index) {
+    return AnimatedBuilder(
+        animation: _pageController,
+        builder: (context, child) {
+          double value = 1;
+          if (_pageController.position.haveDimensions) {
+            value = _pageController.page - index;
+            value = (1 - (value.abs() * 0.5)).clamp(0.0, 1.0);
+          }
+          return Expanded(
+            child: Transform.translate(
+              offset: Offset(0, 300 - value * 300),
+              child: Opacity(
+                opacity: (value - 0.7) * 3,
+                child: Container(
+                  padding:
+                      EdgeInsets.only(left: 25.0, right: 25.0, bottom: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        detailsList[index].title,
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        detailsList[index].description,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Spacer(),
+                      ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child: Container(
+                          width: 80.0,
+                          height: 5.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Read More',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w900),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
   }
 
   Widget itemBuilder(index) {
@@ -61,15 +139,26 @@ class _CityExplorePageState extends State<CityExplorePage> {
         if (_pageController.position.haveDimensions) {
           value = _pageController.page - index;
           value = (1 - (value.abs() * 0.5)).clamp(0.0, 1.0);
-        } else {}
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-            height: Curves.easeIn.transform(value) * 300,
-            child: child,
-          ),
-        );
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              height: Curves.easeIn.transform(value) * 450,
+              child: child,
+            ),
+          );
+        } else {
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              height:
+                  Curves.easeIn.transform(value) *
+                      450,
+              child: child,
+            ),
+          );
+        }
       },
       child: Material(
         elevation: 5.0,
@@ -78,7 +167,7 @@ class _CityExplorePageState extends State<CityExplorePage> {
                 bottomLeft: Radius.circular(8.0),
                 bottomRight: Radius.circular(8.0))),
         child: Padding(
-          padding: const EdgeInsets.only(left: 5.0, right: 5.0, bottom: 5.0),
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
           child: ClipRRect(
               borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(9.0),
